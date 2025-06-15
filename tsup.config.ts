dd05b532ca8164356d1cadf03e32bab70e0d415e
Artandrey <artandrey7777@gmail.com>
2025-06-15 05:17:49 +0300

    fix(bullmq): dist empty

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     'core/dist/index': './packages/core/lib/index.ts',
+    'bullmq/dist/index': './packages/bullmq/lib/index.ts',
   },
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   clean: false,
   minify: true,
