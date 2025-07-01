412cc8ceab7c1965bc073dce251795642bce6ca6
Artandrey <artandrey7777@gmail.com>
2025-07-01 22:52:11 +0300

    test(bullmq): update Redis container usage in e2e tests

--- packages/bullmq/tests/e2e/bull-mq-e2e.spec.ts
+++ packages/bullmq/tests/e2e/bull-mq-e2e.spec.ts
@@ -5,22 +5,21 @@ import {
   EventHandler,
   HandlerRegister,
 } from '@event-driven-architecture/core';
-import { RedisContainer } from '@testcontainers/redis';
-import { Queue } from 'bullmq';
+import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
+import { ConnectionOptions, Queue } from 'bullmq';
 import {
   AtomicBullMqEventPublisher,
   BulkBullMqEventPublisher,
   BullMqEvent,
   BullMqEventConsumerService,
   EventsRegisterService,
+  FanoutRouter,
   FlowRegisterService,
   QueueRegisterService,
   WorkerRegisterService,
   WorkerService,
 } from 'packages/bullmq/lib';
 import { HandlesBullMq } from 'packages/bullmq/lib/util';
-import { StartedTestContainer } from 'testcontainers';
-import { ConnectionOptions } from 'tls';
 
 describe.each([
   {
@@ -30,9 +29,8 @@ describe.each([
     publisher: BulkBullMqEventPublisher,
   },
 ])('BullMQ E2E', ({ publisher }) => {
-  let redisContainer: StartedTestContainer;
-  let redisHost: string;
-  let redisPort: number;
+  let redisContainer: StartedRedisContainer;
+  let connectionUrl: string;
 
   let workerRegisterService: WorkerRegisterService;
   let queueRegisterService: QueueRegisterService;
@@ -41,25 +39,24 @@ describe.each([
   let eventBus: EventBus;
   let handlerRegister: HandlerRegister;
   let eventConsumer: BullMqEventConsumerService;
-
+  let fanoutRouter: FanoutRouter;
   const QUEUE_NAME = 'queue';
   const JOB_NAME = 'job';
 
   beforeEach(async () => {
-    redisContainer = await new RedisContainer().start();
+    redisContainer = await new RedisContainer('redis:7.2').start();
 
-    redisHost = redisContainer.getHost();
-    redisPort = redisContainer.getFirstMappedPort();
+    connectionUrl = redisContainer.getConnectionUrl();
     workerRegisterService = new WorkerRegisterService();
     queueRegisterService = new QueueRegisterService();
     eventsRegisterService = new EventsRegisterService();
     flowRegisterService = new FlowRegisterService();
     handlerRegister = new BaseHandlerRegister();
     eventBus = new BaseEventBus(handlerRegister);
+    fanoutRouter = new FanoutRouter();
 
     const CONNECTION: ConnectionOptions = {
-      host: redisHost,
-      port: redisPort,
+      url: connectionUrl,
     };
 
     queueRegisterService.add(new Queue(QUEUE_NAME, { connection: CONNECTION }));
@@ -113,7 +110,7 @@ describe.each([
     handlerRegister.addHandler(HandlesBullMq(TestEvent), new TestHandler());
     eventConsumer.init();
 
-    const eventPublisher = new publisher(queueRegisterService, flowRegisterService);
+    const eventPublisher = new publisher(queueRegisterService, flowRegisterService, fanoutRouter);
     // later replace with BaseEventBus<BullMqEvent>
     (eventBus as any).publisher = eventPublisher;
 
